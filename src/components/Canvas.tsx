f5f1cbea9d1ad83ba5dae24643cbfe2e090b6fc5
David Lee <dav@davlee.com>
2024-12-09 10:49:37 +1100

    canvas element, vite, bun, react

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,8 @@
+export default function Canvas() {
+    return (
+      <div id="scene-container">
+        <p>Hello World</p>
+        <canvas id="scene" height={600} width={800} />
+      </div>
+    )
+}
